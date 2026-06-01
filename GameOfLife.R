db908b0194895e253b787f6f3b99a69acd0bbf73
Zero <zero.antonios2293@gmail.com>
2026-06-01 14:44:10 +0200

    add noise generation + changed colours

--- GameOfLife.R
+++ GameOfLife.R
@@ -1,16 +1,19 @@
 ROWS = 20
 COLS = 20
+noise_frequency = 0.15
 
 # We create 2 matrices, the arena and the one to calculate nb of living neighbours 
-arena        <- matrix(data = 0, nrow = ROWS, ncol = COLS, byrow = FALSE, dimnames = NULL)
+# arena        <- matrix(data = 0, nrow = ROWS, ncol = COLS, byrow = FALSE, dimnames = NULL)
 living_neigh <- matrix(data = 0, nrow = nrow(arena), ncol = ncol(arena))
+
+arena <- pmax(sign(noise_perlin(c(ROWS,COLS),frequency = noise_frequency)), 0)
 
 arena[2:7, 3:6] = 1 # Init cell block of 2-7 x 3-6 as alive?
 
 run_iter <- function(arena) {
   # Put the nb of living neighbours for each cell in the corresponding one in that matrix
-  for (R in 2:11) { # why 2:11??
-    for (C in 2:11) {
+  for (R in 2:18) { # why 2:11??
+    for (C in 2:18) {
       living_neigh[R,C] <- (
         as.numeric(arena[R-1,C-1])
         + as.numeric(arena[R-1, C])
@@ -25,8 +28,8 @@ run_iter <- function(arena) {
   }
   
   # calculate if alive or dead for next gen
-  for (R in 2:11) {
-    for (C in 2:11) {
+  for (R in 2:18) {
+    for (C in 2:18) {
       if (arena[R,C] == 1) { # Cell is alive
         if (living_neigh[R,C] > 1 && living_neigh[R,C] < 4) {
           arena[R,C] <- 1
@@ -49,12 +52,12 @@ run_iter <- function(arena) {
 
 display_arena = function(arena) {
   fixed_arena = t(apply(arena, 2, rev)) 
-  image(fixed_arena, col = c("black", "white"), axes = FALSE)
+  image(fixed_arena, col = c("purple", "skyblue"), axes = FALSE)
 }
 
 display_arena(arena)
 
-for (generation in 1:5) {
+for (generation in 1:50) {
   arena <- run_iter(arena)
   Sys.sleep(0.2)
   print(generation)
