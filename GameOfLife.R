cadba678b36b01163c475a749bfe657a65cbe38e
Niels <nielsz@pm.me>
2026-06-01 10:32:54 +0200

    clean up

--- GameOfLife.R
+++ GameOfLife.R
@@ -1,16 +1,17 @@
-
-#We create 2 matrices, the arena and the one to calculate nb of living neighbors 
-arena <- matrix(data = 0, nrow = 20, ncol = 20, byrow = FALSE, dimnames = NULL)
+# We create 2 matrices, the arena and the one to calculate nb of living neighbours 
+arena        <- matrix(data = 0, nrow = 20, ncol = 20, byrow = FALSE, dimnames = NULL)
 living_neigh <- matrix(data = 0, nrow = nrow(arena), ncol = ncol(arena))
 
-arena[2:7,3:6]=1
-#Put the nb of living neighbors for each cell in the corresponding one in that matrix
+arena[2:7, 3:6] = 1
+
+# Put the nb of living neighbours for each cell in the corresponding one in that matrix
 for (R in 2:11) {
   for (C in 2:11) {
     living_neigh[R,C] <- as.numeric(arena[R-1,C-1]) + as.numeric(arena[R-1, C]) + as.numeric(arena[R-1, C+1]) + as.numeric(arena[R,C-1]) + as.numeric(arena[R, C+1]) + as.numeric(arena[R+1,C-1]) + as.numeric(arena[R+1, C]) + as.numeric(arena[R+1, C+1])
   }
 }
-#calculate if alive or dead for next gen
+
+# calculate if alive or dead for next gen
 for (R in 2:11) {
   for (C in 2:11) {
     if (arena[R,C]==1) {
